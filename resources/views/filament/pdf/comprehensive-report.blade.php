aa57d173ed4c9de10ea71d4ff948f93f3bfe31a7
meeranas <meersaria01@gmail.com>
2025-09-24 22:10:14 +0500

    Fix

--- resources/views/filament/pdf/comprehensive-report.blade.php
+++ resources/views/filament/pdf/comprehensive-report.blade.php
@@ -88,7 +88,7 @@
         }
 
         .summary-card {
-            width: 16.5%;
+            width: 10%;
             background: #ffffff;
             border: 1px solid #e2e8f0;
             padding: 20px 15px;
